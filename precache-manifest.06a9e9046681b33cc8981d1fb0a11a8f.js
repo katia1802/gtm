c2b2a3a943199983e7d227618c84d69a275d740e
katia1802 <katia.rojas.sandoval@gmail.com>
2020-08-13 15:50:13 +0200

    Updates

---
+++ precache-manifest.06a9e9046681b33cc8981d1fb0a11a8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6808c024ddf08aabaa8414459913e775",
+    "url": "/gtm/index.html"
+  },
+  {
+    "revision": "c707515a3d80a6a5230a",
+    "url": "/gtm/static/css/2.11976b7f.chunk.css"
+  },
+  {
+    "revision": "d88d02722b68150ef65e",
+    "url": "/gtm/static/css/main.686c7909.chunk.css"
+  },
+  {
+    "revision": "c707515a3d80a6a5230a",
+    "url": "/gtm/static/js/2.04b1764a.chunk.js"
+  },
+  {
+    "revision": "e163ddb0b294eeadd4e3264a811f9d51",
+    "url": "/gtm/static/js/2.04b1764a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88d02722b68150ef65e",
+    "url": "/gtm/static/js/main.489dc294.chunk.js"
+  },
+  {
+    "revision": "9c6b2ca2fb6badaf78f0",
+    "url": "/gtm/static/js/runtime-main.d6af5eb8.js"
+  },
+  {
+    "revision": "a69839fde8f40456ae010dd273e6eede",
+    "url": "/gtm/static/media/Workspace.a69839fd.jpg"
+  }
+]);
