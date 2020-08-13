20b778ef880a4d17fab44029170a5a833287fbd0
katia1802 <katia.rojas.sandoval@gmail.com>
2020-08-13 17:00:21 +0200

    update gitignore

---
+++ build/precache-manifest.324850d853d379d20325bbc0dd6befe5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5617ac8a2546d4ada22dc4b4f2072b",
+    "url": "/gtm/index.html"
+  },
+  {
+    "revision": "dad97e4e615589d58b54",
+    "url": "/gtm/static/css/2.11976b7f.chunk.css"
+  },
+  {
+    "revision": "fda6a724a10eae6a3416",
+    "url": "/gtm/static/css/main.686c7909.chunk.css"
+  },
+  {
+    "revision": "dad97e4e615589d58b54",
+    "url": "/gtm/static/js/2.07453a6d.chunk.js"
+  },
+  {
+    "revision": "e163ddb0b294eeadd4e3264a811f9d51",
+    "url": "/gtm/static/js/2.07453a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda6a724a10eae6a3416",
+    "url": "/gtm/static/js/main.1fe1ba9c.chunk.js"
+  },
+  {
+    "revision": "9c6b2ca2fb6badaf78f0",
+    "url": "/gtm/static/js/runtime-main.d6af5eb8.js"
+  },
+  {
+    "revision": "a69839fde8f40456ae010dd273e6eede",
+    "url": "/gtm/static/media/Workspace.a69839fd.jpg"
+  }
+]);
